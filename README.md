7bcc6dd6ffff69d3c7296bc26187dfe4052faed3
GohJiaDong <s10243744@connect.np.edu.sg>
2022-12-04 02:24:22 +0800

    Week 7 3/12

--- README.md
+++ README.md
@@ -1,37 +1,61 @@
-# IDassignment1Repo
 Assignment 1: Website for Chase Atlantic band
     This webpage project is a platform for the band's music and merchandise. The website promotes the band through their up-coming tours and provides hyperlinks to the band's main music platform (youtube/spotify). The goal of this project is to showcase a webpage as a 'hub' to the band's socials, products and music. 
 
 
 DESIGN
-    Similar to the band's offical website, the purpose of my webpage is to promote their merchadise sales, music and tours. 
 
-    As a fan, if I want find out all of Chase Atlantic's social media, the website should provide me with direct links.
-    As a consumer, I should have access to the up-coming tour details and be able to access all the merchadise on sale.
+Process:
+As a fan, i want to visit a section dedicated to informing site visitors of the members of the band, so i can know more about the members.
+As a fan, i want to be up-to-date with the band, so i should be able to find social media links/others to find the band's account on other platforms.
 
-FEATURE
-Implemented:
+As a consumer, i want to buy merchandise from the band to support them, so i will look for a merchandise/store related section
+As a consumer, i want to support the band through their music, so i will look for links to stream their albums and songs
+As a consumer, if i want to know find out and purchase tickets for an up-coming tour, so i should be able to find the links/information from the website
+
+https://xd.adobe.com/view/bc1de590-e3ff-4a8f-b269-82460bd6e513-e94e/ (zoom out)
+
+Features implemented:
 1) A responsive navigation bar
 2) A dropdown menu for the band's social
 3) A footer with links to the band's social
+    1) An input field that allows site visitors to enter their email to sign up for notifications
 4) Advertisement for the most recent album release
-
-
-Yet to inplement:
-1) A responsive gallary
-2) A responsive tour section
+5) A responsive member section
+6) A responsive tour section
     1) With links to the offical tour website
-3) A functional sign-up feature for the footer
-4) A responsive page for the music released by the band
-    1) Showcases the release date + features by other artists
-    2) Links to both youtube/spotify to listen to the tracks/albums
-5) A responsive page for the merchandise on sale
-    1) Showcases the price (in USD)
+7) A responsive page for the music released by the band
+    1) Links to youtube/spotify to listen to the tracks/albums
+8) A responsive page for the merchandise on sale
+    1) Showcases the price
+    2) Able to add items to cart and showcase a mock-up cart feature
+
+
+TESTING
+1) Email registration for notification
+    i. Scroll down to the footer to find
+    ii.  Submit an input that doesn't consist of '@' and '.com' or an empty field. Verify that an alert regarding  the error appears.
+    iii. Submit an email which consist of both '@' and '.com'. Verify that an alert to notify user about the valid email appears.
+2) Navigation bar and Footer
+    i. On desktop view, each section's items should spread evenly to occupy as much screen space as possible
+    ii. On mobile view, those items should be as compact as possible.
+3) Merch page
+    i. Add different items into the cart by pressing the blue 'cart' icon below each items.
+    ii. Scroll down to find the cart button. Press once to toggle it to open.
+    iii. If there are too many items, the cart should be scrollable, the grand total price and quantity should not be affected.
+    iv. Hover and press the cross corresponding to each item, that item should be removed from the cart. the grand total price and quantity should change.
+    v. Hover and press the green 'checkout' icon, everything should be cleared from the cart. Verify that an alert regarding the checkout appears. Note that this is a mock up that should be assumed to already know the checkout details.
+
 
 CREDITS
 
 Acknowledge:
 Inspirations taken from the official Chase Atlantic website - chaseatlantic.us chaseatlantic.com
+
+Content:
+Information about the band (members) - https://celebs.filmifeed.com/wiki/mitchel-cave/
+                                       https://celebs.filmifeed.com/wiki/clinton-cave/
+                                       https://celebs.filmifeed.com/wiki/christian-anthony/
+                                       https://www.nextbiography.com/christian-anthony/
 
 Media:
 chase-logo https://chaseatlantic.us/
@@ -39,7 +63,12 @@ fb-logo https://www.transparentpng.com/cats/facebook-logo-1901.html
 ig-logo https://parspng.com/en/images/instagram-black-white-logo-png/
 yt-logo https://www.flaticon.com/free-icon/youtube-symbol_48968
 beauty-in-death-logo https://beautyindeathc3.com/
-featured-gif https://giphy.com/gifs/chaseatlantic-molly-chase-atlantic-out-the-roof-bpQXx55XzBfhbxmlth/fullscreen
+featured2-gif https://giphy.com/gifs/chaseatlantic-molly-chase-atlantic-out-the-roof-bpQXx55XzBfhbxmlth/fullscreen
 mc-photo https://www.flickr.com/photos/peterhutchins/43231217874
 cc-photo https://genius.com/artists/Clinton-cave
 ca-photo https://weheartit.com/entry/333881933#
+checkout-icon https://thenounproject.com/browse/icons/term/proceed-to-checkout/?iconspage=1
+cart-icon https://www.flaticon.com/free-icon/shopping-cart_1413908
+album covers https://genius.com/artists/Chase-atlantic
+merchandise item photos chaseatlantic.us
+
---
+++ merch.js
@@ -0,0 +1,202 @@
+/*Toggle between showing and hiding cart*/
+function openCart() {
+    document.getElementById("cart").style.display = "block";
+    document.getElementById("cartsummary").style.display = "block";
+  }
+  
+function closeCart() {
+    document.getElementById("cart").style.display = "none";
+    document.getElementById("cartsummary").style.display = "none";
+  }
+
+/*Cart content*/
+var itemlist = new Array();
+var pricelist = new Array ();
+let totalcost = 0;
+let a = '';
+
+
+function merch(name, price) {
+    this.name = name;
+    this.price = price;
+    this.addtocart = function() {
+        itemlist.push(this.name);
+        pricelist.push(this.price);
+        totalcost += this.price;
+
+        /* Update the cart */
+        var target = document.querySelector(".cart");
+
+        let newUL = document.createElement("ul");
+        newUL.classList.add("cartitem");
+
+        if (this.name == 'CHASE ATLANTIC STARS T-SHIRT') {
+          a = 'item1.removefromcart()'
+        }
+        else if (this.name == 'CHASE ATLANTIC RESERVED') {
+          a = 'item2.removefromcart()'
+        }
+        else if (this.name == 'CHASE ATLANTIC GUARENTEED HOODIE') {
+          a = 'item3.removefromcart()'
+        }
+        else if (this.name == 'CHASE ATLANTIC UNLICENSED TOUR T-SHIRT') {
+          a = 'item4.removefromcart()'
+        }
+        else if (this.name == 'CHASE ATLANTIC LIVE IN CONCERT TOUR T-SHIRT') {
+          a = 'item5.removefromcart()'
+        }
+        else if (this.name == 'CHASE ATLANTIC BLUR TOUR T-SHIRT') {
+          a = 'item6.removefromcart()'
+        }
+        else if (this.name == 'CHASE ATLANTIC FACE TOUR HOODIE') {
+          a = 'item7.removefromcart()'
+        }
+        else if (this.name == 'CHASE ATLANTIC FACE CREWNECK TOUR') {
+          a = 'item8.removefromcart()'
+        }
+        else if (this.name == 'CHASE ATLANTIC XAVIER MAYNE SHORTY SAID SHE T-SHIRT') {
+          a = 'item9.removefromcart()'
+        }
+        else if (this.name == 'Chase Atlantic Phases Black Hoodie') {
+          a = 'item10.removefromcart()'
+        }
+        else if (this.name == 'Chase Atlantic BEAUTY IN DEATH Hoodie') {
+          a = 'item11.removefromcart()'
+        }
+        else if (this.name == 'Chase Atlantic Smile Hoodie') {
+          a = 'item12.removefromcart()'
+        }
+        else if (this.name == 'CHASE ATLANTIC COLLEGIATE T-SHIRT') {
+          a = 'item13.removefromcart()'
+        }
+        else if (this.name == 'CHASE ATLANTIC COLLEGIATE CREWNECK SWEATSHIRT') {
+          a = 'item14.removefromcart()'
+        }
+        else if (this.name == 'CHASE ATLANTIC BEAUTY IN DEATH TOUR FACE T-SHIRT') {
+          a = 'item15.removefromcart()'
+        }
+        else if (this.name == 'CHASE ATLANTIC BEAUTY IN DEATH TOUR AUTHENTIC T-SHIRT') {
+          a = 'item16.removefromcart()'
+        }
+        else if (this.name == 'CHASE ATLANTIC - BEAUTY IN DEATH TOUR HOODIE') {
+          a = 'item17.removefromcart()'
+        }
+        else if (this.name == 'Chase Atlantic - New Rari Hoodie') {
+          a = 'item18.removefromcart()'
+        }
+        else if (this.name == 'Chase Atlantic - Ohmami Crewneck') {
+          a = 'item19.removefromcart()'
+        }
+        else if (this.name == 'Chase Atlantic - One Fifty T-shirt') {
+          a = 'item20.removefromcart()'
+        }
+        else if (this.name == 'Chase Atlantic - New Rari T-Shirt') {
+          a = 'item21.removefromcart()'
+        }
+        else if (this.name == 'Molly Hoodie 3D') {
+          a = 'item22.removefromcart()'
+        }
+        else if (this.name == 'Molly Crewneck Sweatshirt') {
+          a = 'item23.removefromcart()'
+        }
+        else if (this.name == 'Out The Roof Hoodie') {
+          a = 'item24.removefromcart()'
+        }
+        else {
+          a = 'item25.removefromcart()'
+        }
+
+        let newLi3 = document.createElement("li");
+        newLi3.classList.add("removeitem");
+        let newBt = document.createElement("button");
+        newBt.setAttribute('onclick', a);
+        let NewC3 = document.createTextNode("X")
+        newBt.appendChild(NewC3);
+        newLi3.appendChild(newBt);
+        newUL.appendChild(newLi3);
+
+        let newLi1 = document.createElement("li");
+        newLi1.classList.add("itemname");
+        let NewC1 = document.createTextNode(`${this.name}`)
+        newLi1.appendChild(NewC1);
+        newUL.appendChild(newLi1);
+
+        let newLi2 = document.createElement("li");
+        newLi2.classList.add("itemprice");
+        let NewC2 = document.createTextNode(`$${this.price}`)
+        newLi2.appendChild(NewC2);
+        newUL.appendChild(newLi2);
+
+        target.appendChild(newUL);
+
+        /* Update the cart summary */
+        var newQty = itemlist.length;
+        document.getElementById("itemqty").innerHTML = "Quantity:<br>"+newQty;
+
+        var newGT = totalcost;
+        document.getElementById("grandtotal").innerHTML = "Grand Total:<br>$"+newGT;
+    };
+    this.removefromcart = function() {
+        var i = itemlist.indexOf(this.name);
+        if (i == -1) {
+          return
+        }
+        itemlist.splice(i, 1);
+        pricelist.splice(i, 1);
+        totalcost -= this.price;
+        /* Update the cart */
+        var target = document.querySelector(".cart");
+        var target2 = document.querySelectorAll(".cartitem");
+        target.removeChild(target2[i+1]);
+        /* Update the cart summary */
+        var newQty = itemlist.length;
+        document.getElementById("itemqty").innerHTML = "Quantity:<br>"+newQty;
+
+        var newGT = totalcost;
+        document.getElementById("grandtotal").innerHTML = "Grand Total:<br>$"+newGT;
+    };
+};
+
+let item1 = new merch('CHASE ATLANTIC STARS T-SHIRT', 25);
+let item2 = new merch ('CHASE ATLANTIC RESERVED', 25);
+let item3 = new merch ('CHASE ATLANTIC GUARENTEED HOODIE', 55);
+let item4 = new merch ('CHASE ATLANTIC UNLICENSED TOUR T-SHIRT', 25);
+let item5 = new merch ('CHASE ATLANTIC LIVE IN CONCERT TOUR T-SHIRT', 25);
+let item6 = new merch ('CHASE ATLANTIC BLUR TOUR T-SHIRT', 25);
+let item7 = new merch ('CHASE ATLANTIC FACE TOUR HOODIE', 55);
+let item8 = new merch ('CHASE ATLANTIC FACE CREWNECK TOUR', 45);
+let item9 = new merch ('CHASE ATLANTIC XAVIER MAYNE SHORTY SAID SHE T-SHIRT', 25);
+let item10 = new merch ('Chase Atlantic Phases Black Hoodie', 55);
+let item11 = new merch ('Chase Atlantic BEAUTY IN DEATH Hoodie', 55);
+let item12 = new merch ('Chase Atlantic Smile Hoodie', 55);
+let item13 = new merch ('CHASE ATLANTIC COLLEGIATE T-SHIRT', 25);
+let item14 = new merch ('CHASE ATLANTIC COLLEGIATE CREWNECK SWEATSHIRT', 45);
+let item15 = new merch ('CHASE ATLANTIC BEAUTY IN DEATH TOUR FACE T-SHIRT', 25);
+let item16 = new merch ('CHASE ATLANTIC BEAUTY IN DEATH TOUR AUTHENTIC T-SHIRT', 25);
+let item17 = new merch ('CHASE ATLANTIC - BEAUTY IN DEATH TOUR HOODIE', 55);
+let item18 = new merch ('Chase Atlantic - New Rari Hoodie', 55);
+let item19 = new merch ('Chase Atlantic - Ohmami Crewneck', 45);
+let item20 = new merch ('Chase Atlantic - One Fifty T-shirt', 30);
+let item21 = new merch ('Chase Atlantic - New Rari T-Shirt', 30);
+let item22 = new merch ('Molly Hoodie 3D', 55);
+let item23 = new merch ('Molly Crewneck Sweatshirt', 45);
+let item24 = new merch ('Out The Roof Hoodie', 55);
+let item25 = new merch ('Chase Atlantic Tee', 25);
+
+function checkout () {
+  alert("Merchandise purchase successful!");
+  itemlist = [];
+  pricelist = [];
+  totalcost = 0;
+
+  document.getElementById("itemqty").innerHTML = "Quantity:<br>0";
+  document.getElementById("grandtotal").innerHTML = "Grand Total:<br>-";
+  /* Update the cart */
+  var target3 = document.querySelector(".cart");
+  var target4 = document.querySelectorAll(".cartitem");
+  for (let i = 1; i < target4.length; i++)
+  {
+    target3.removeChild(target4[i]); 
+  }
+  /* Update the cart summary */
+};
